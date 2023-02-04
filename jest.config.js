b9b98b607fe45c9829e0f2445a79b8a0e33af2ce
Yaroslavcher <yaroslavcher@gmail.com>
2023-02-04 23:48:55 +0300

    first commit

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     verbose: true, //указывает на то, что каждый тест будет показан в отчете о процессе запуска
     preset: "jest-puppeteer", //указываем, что будем использовать эту библиотеку
-    testTimeout: 10000,
+    testTimeout: 20000,
 };
+
+setupFilesAfterEnv: ['./jest-setup.js']
--- ticket.test.js
+++ ticket.test.js
@@ -1,8 +1,12 @@
+const {clickElement, getText} = require("./lib/commands");
+
 let page;
 
 beforeEach(async () => {
     page = await browser.newPage();
     await page.goto("http://qamid.tmweb.ru/client/index.php");
+    await clickElement(page, "a:nth-child(2) > span.page-nav__day-week"); // click on date
+    await clickElement(page, "a.movie-seances__time");  // click on time
 });
 
 afterEach(() => {
@@ -10,17 +14,40 @@ afterEach(() => {
 });
 
 test("Should book one ticket", async () => {
-    const movieTime = "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li > a";
+    await clickElement(page, ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken)"); // click on chair
+    await clickElement(page, "button.acceptin-button"); //click on button "Забронировать"
+
+    const actual = await getText(page, "h2.ticket__check-title");
+    expect(actual).toContain("Вы выбрали билеты:");
+});
+    
+test("Should book two tickets", async () => {
+        // const movieTime = "li:nth-child(2) > a.movie-seances__time";
+        // await page.waitForSelector(movieTime);
+        // await page.click(movieTime);
+        // await page.waitForTimeout(2000);
+    await clickElement(page, ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken)"); // click on chair
+    await page.waitForTimeout(2000);
+    await clickElement(page, ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken):not(.buying-scheme__chair_selected)");   
+        
+    await clickElement(page, "button.acceptin-button");
+        //await page.waitForTimeout(2000);
+    const actual = await getText(page, "h2.ticket__check-title");
+    expect(actual).toContain("Вы выбрали билеты:");
+});
+
+
+test("Should try to book taken chair", async () => {
+    await page.waitForTimeout(2000);
+    const movieTime = "section:nth-child(2) > div.movie-seances__hall > ul > li > a";
     await page.waitForSelector(movieTime);
     await page.click(movieTime);
-    await page.waitForTimeout(2000);
-    const chair = "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(3) > span:nth-child(1)";
-    
+    await page.waitForTimeout(3000);
+    const chair = "div > .buying-scheme__chair_taken";
+       
     await page.waitForSelector(chair);
     await page.click(chair);
-    
-const btn = await page.$("body > main > section > button");
-    await btn.click();
-    const selectedTicketHeader = await page.$eval("h2.ticket__check-title", (link) => link.textContent);
-    expect(selectedTicketHeader).toContain("Вы выбрали билеты");
+    const actual = await page.$eval(".acceptin-button", (link) => link.getAttribute("disabled"));
+ 
+    expect(actual).toEqual("true");
 });  
